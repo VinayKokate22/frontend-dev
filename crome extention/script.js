ed0dde6322cdda8a90f999d2fc610a2d7b3eb743
Vinay Kokate <vinaynkokate@gmail.com>
2023-01-11 23:08:04 +0530

    basic structure completed

--- crome extention/script.js
+++ crome extention/script.js
@@ -4,28 +4,53 @@ const deleteBtn=document.getElementById("delete-btn")
 
 const inputEl=document.getElementById("input-el")
 const ulEl=document.getElementById("ul-el")
+const tabBtn=document.getElementById("tab-btn")
 const leadsFromlocalstorage=JSON.parse(localStorage.getItem("myleads"))
+
+if (leadsFromlocalstorage) {
+    myLeads=leadsFromlocalstorage
+    render(myLeads)
+}
+
+
+
+
+tabBtn.addEventListener("click",function () {
+
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myleads",JSON.stringify(myLeads))
+        render(myLeads)
+
+    })
+
+   
+
+})
 inputBtn.addEventListener("click", function(){
     
-    console.log("helo")
+    // console.log("helo")
     myLeads.push(inputEl.value)
     if (inputEl.value!="") {
         inputEl.value=""
     }
+     
+    localStorage.setItem("myleads",JSON.stringify(myLeads))
 
-    renderleads()
+    render(myLeads)
 })
 
 deleteBtn.addEventListener("click",function () {
     localStorage.clear()
     myLeads=[]
-    renderleads()
+    render(myLeads)
 })
 
-function renderleads(){
+function render( leads){
     let listItems=""
-for (let i = 0; i < myLeads.length; i++) {
-    listItems+= "<li><a href='#' target='_blank'>"+myLeads[i]+"</a></li>";
+for (let i = 0; i < leads.length; i++) {
+    listItems+= "<li><a href='#' target='_blank'>"+leads[i]+"</a></li>";
     
 }
 ulEl.innerHTML=listItems
