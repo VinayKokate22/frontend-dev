87b6a83922fd1c3b852a87c606925ca90d997352
Vinay Kokate <vinaynkokate@gmail.com>
2023-01-18 13:15:46 +0530

    tranisition not working

--- tailwind/navbar/dist/script.js
+++ tailwind/navbar/dist/script.js
@@ -1,8 +1,9 @@
 const burgerbutton=document.querySelector(".burger-button")
-const hidden=document.querySelector("#hidden") 
-const higthofnavBar=document.querySelector(".heightofnavbar") 
+const hidden=document.querySelector(".hidden") 
+const higthofnavBar=document.querySelector(".header") 
 
 burgerbutton.addEventListener("click",function () {
+    console.log("function is working");
     hidden.classList.toggle("hidden")
-    higthofnavBar.classList.toggle("h-52")
+    higthofnavBar.classList.toggle("h-16")
 })
