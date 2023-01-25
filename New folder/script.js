7911dc78d600167195deeee237907041361585e0
Vinay Kokate <vinaynkokate@gmail.com>
2023-01-25 21:01:29 +0530

    multiple color added

--- New folder/script.js
+++ New folder/script.js
@@ -6,6 +6,41 @@ const tabLinks= document.querySelector(".tab-links-skills")
 const tabLinksExp= document.querySelector(".tab-links-exp")
 const tabLinksEdu= document.querySelector(".tab-links-edu")
 
+const colorPalet=document.querySelector(".colorpalet")
+const colorOption=document.querySelector(".color-option")
+
+colorPalet.addEventListener("click",function () {
+    colorOption.classList.toggle("hidden")
+})
+
+const Colors=document.querySelector(".red")
+Colors.addEventListener("click",function () {
+            console.log("hello")
+            let color=this.getAttribute('data-color')
+            document.documentElement.style.setProperty('--colortheme',color)
+    colorOption.classList.toggle("hidden")
+
+        })
+
+        const Colors2=document.querySelector(".blue")
+        Colors2.addEventListener("click",function () {
+                    console.log("hello")
+                    let color=this.getAttribute('data-color')
+                    document.documentElement.style.setProperty('--colortheme',color)
+    colorOption.classList.toggle("hidden")
+
+                })  
+                
+                const Colors3=document.querySelector(".green")
+Colors3.addEventListener("click",function () {
+            console.log("hello")
+            let color=this.getAttribute('data-color')
+            document.documentElement.style.setProperty('--colortheme',color)
+    colorOption.classList.toggle("hidden")
+
+        })
+
+
 
 tabLinks.addEventListener("click",function () {
     aboutSkills.classList.add("active-tab")
